f078404d737f2fc2988a939608db23d4a4e5b00f
MatejSmith <71751801+MatejSmith@users.noreply.github.com>
2020-09-30 08:36:36 +0200

    Update Exercise.cs

--- Open-Lab-01.02/Exercise.cs
+++ Open-Lab-01.02/Exercise.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_01._02
 {
@@ -6,7 +6,9 @@ namespace Open_Lab_01._02
     {
         public int Sum(int a, int b)
         {
-            throw new NotImplementedException();
+            int celkom;
+            celkom = a + b;
+            return celkom;
         }
     }
 }
